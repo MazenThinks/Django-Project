13ba65ef68d6866588efb590db4e23268b24be81
Mazen Magdy <mazenmagdy100.mm@gmail.com>
2021-12-17 19:37:32 +0200

    updated urls

--- src/tutorial/urls.py
+++ src/tutorial/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from django.urls import include, path
+from rest_framework import routers
+from tutorial.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [  path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
